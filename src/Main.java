af36554d6b557b347a880652422bd331dfeb5a73
Jaydesire <elgovsky@ya.ru>
2022-04-17 17:24:40 +0300

    Add openZip and openProgress

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,50 @@
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        openZip("src\\savegames\\zipFile.zip", "src\\savegames");
+        openProgress("src\\savegames\\OpennedZip\\save1.dat");
+    }
 
-        
+    static void openProgress(String pathToFileSave) {
+        GameProgress gameProgress = null;
 
+        try (FileInputStream fileInputStream = new FileInputStream(pathToFileSave);
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            gameProgress = (GameProgress) objectInputStream.readObject();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(gameProgress);
+    }
+
+    static void openZip(String pathToArchiveFolder, String pathToFolderForUnzippedFiles) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(pathToArchiveFolder))) {
+
+            File dirOpennedZip = new File(pathToFolderForUnzippedFiles + "/OpennedZip/");
+            dirOpennedZip.mkdir();
+
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();// получим название файла
+                // распаковка
+                FileOutputStream fout = new FileOutputStream(pathToFolderForUnzippedFiles + "/OpennedZip/" + name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                System.out.println("Распакован файл " + name);
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
